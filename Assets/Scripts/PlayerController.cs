223f8acdd929bbdd843c70ea5def45913a42a3e5
Griffin Hammer <grifhammer@gmail.com>
2015-06-18 15:08:33 -0400

    First Commit!

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public Text countText;
+	public Text winText;
+
+	private Rigidbody rb;
+	private int count;
+
+	void Start(){
+		count = 0;
+		SetCountText ();
+		winText.text = "";
+		rb = GetComponent<Rigidbody> ();
+	}
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.CompareTag ("Pickup"))
+		{
+			count++;
+			SetCountText ();
+			other.gameObject.SetActive (false);
+		}
+	}
+	void FixedUpdate(){
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0, moveVertical);
+
+		rb.AddForce (movement * speed);
+	}
+
+	void SetCountText(){
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 12) {
+			winText.text = "YOU WIN!";
+		}
+
+	}
+
+}
